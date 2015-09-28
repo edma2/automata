deadf7143baa68213f4c9b8f81c3c865500be1fe
Eugene Ma <ema@twitter.com>
2015-09-27 17:15:28 -0700

    A toy implementation of DFAs
    Later:
    Define a mutable "builder" interface for ɛ-NFAs, which produces
    immutable, executable DFA.
    
    Why ɛ-NFA?
    They are more general than NFA, which in turn are more general than DFA.
    Everything eventually can be "compiled" down to a DFA

--- dfa/dfa.go
+++ dfa/dfa.go
@@ -1,9 +1,53 @@
 package dfa
 
+// A deterministic finite automaton M is a 5-tuple, (Q, Σ, δ, q0, F), consisting of
+
+// a finite set of states (Q)
+type state string
+
+// a finite set of input symbols called the alphabet (Σ)
+type symbol rune
+
+// a transition function (δ : Q × Σ → Q)
+type transitionTable map[symbol]state
+type transitionFunc map[state]transitionTable
+
+// a start state (q0 ∈ Q)
+// a set of accept states (F ⊆ Q)
 type DFA struct {
-	// empty
+	delta  transitionFunc
+	start  state
+	accept map[state]bool
 }
 
-func newDFA() *DFA {
-	return new(DFA)
+func NewDFA(startState string, acceptStates []string) *DFA {
+	dfa := new(DFA)
+	dfa.delta = make(transitionFunc)
+	dfa.start = state(startState)
+	dfa.accept = make(map[state]bool)
+	for i := 0; i < len(acceptStates); i++ {
+		dfa.accept[state(acceptStates[i])] = true
+	}
+	return dfa
+}
+
+func (dfa *DFA) NewTransition(old string, input rune, new string) {
+	transition := dfa.delta[state(old)]
+	if transition == nil {
+		transition = make(map[symbol]state)
+		dfa.delta[state(old)] = transition
+	}
+	transition[symbol(input)] = state(new)
+}
+
+func (dfa *DFA) Execute(input string) bool {
+	state := dfa.start
+	for _, runeValue := range input {
+		state = dfa.delta[state][symbol(runeValue)]
+		// empty transition
+		if state == "" {
+			return false
+		}
+	}
+	return dfa.accept[state]
 }
--- main.go
+++ main.go
@@ -7,6 +7,18 @@ import (
 )
 
 func main() {
-	a := dfa.DFA{}
-	fmt.Println(a)
+	// DFA with no consecutive 1's
+	d := dfa.NewDFA("A", []string{"A", "B"})
+	d.NewTransition("A", '0', "A")
+	d.NewTransition("A", '1', "B")
+	d.NewTransition("B", '0', "A")
+	d.NewTransition("B", '1', "C")
+	d.NewTransition("C", '0', "C")
+	d.NewTransition("C", '1', "C")
+
+	fmt.Println(d.Execute("011"))
+	fmt.Println(d.Execute("0101"))
+	fmt.Println(d.Execute(""))
+	fmt.Println(d.Execute("01010100001010111"))
+	fmt.Println(d.Execute("000000000000"))
 }
